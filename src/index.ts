ac7474f55ebda57a4ed903a897bbade66217275e
insafly <insa.fly@gmail.com>
2019-10-01 18:25:31 +0300

    hw-1 v2

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,23 @@
 
-// Реализовать метод принимающий строку и возвращающий является ли она полиндромом
+// palindrome
 const checkString = (str: string): boolean => {
     let palindrome: string = str.split('').reverse().join('');
 
-    if (palindrome === str) return true;
+    if (palindrome === str) { return true; }
     return false;
 }
 
-console.log('tratata', checkString('tratata'))
-console.log('tratatart', checkString('tratatart'))
+// console.log('tratata', checkString('tratata'))
+// console.log('tratatart', checkString('tratatart'))
 
-// реализовать метод который принимает число натуральное(int >0)и выдает массив чисел фибоначи до заданного числа
-// fn(2) => [1,1]
-// fn(4) => [1,1,2,3]
+// fibonacci
 
-const getFibonacci = (num: number): Array<number> => {
-    let arr: Array<number> = [];
+const getFibonacci = (num: number): number[] => {
+    const arr: number[] = [];
     let sum: number = 0;
 
-    if (num >= 0) arr.push(0);
-    if (num >= 1) arr.push(1);
+    if (num >= 0) { arr.push(0); }
+    if (num >= 1) { arr.push(1); }
 
     while (num >= sum) {
         sum = arr[arr.length - 1] + arr[arr.length - 2];
@@ -29,5 +27,5 @@ const getFibonacci = (num: number): Array<number> => {
     return arr;
 }
 
-console.log(27, getFibonacci(27));
-console.log(89, getFibonacci(89));
+// console.log(27, getFibonacci(27));
+// console.log(89, getFibonacci(89));
