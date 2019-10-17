9fc4dbf8b702f32228759348b3a0b089e027fe7b
insafly <insa.fly@gmail.com>
2019-10-17 16:33:41 +0300

    hw-6

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <div id="wrapper">
+      <app-promo-component [src]="promoSrc"></app-promo-component>
+      <app-thumb-component (promoIndex)=handler($event)></app-thumb-component>
+    </div>
+  `,
+  styleUrls: ['./app.component.css']
+})
+
+class AppComponent {
+  public promoSrc = '../../assets/img01.jpg';
+
+  public handler($event: number): void {
+    this.promoSrc = `../../assets/img0${ $event + 1 }.jpg`;
+  }
+}
+
+export { AppComponent };
---
+++ src/app/thumb.component/thumb.component.ts
@@ -0,0 +1,32 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+
+@Component({
+    selector: 'app-thumb-component',
+    template: `
+        <ul class="thumbs">
+            <li *ngFor="let thumb of thumbs; let i = index" [class]="i === current ? 'active' : ''">
+                <img (click)="clickHandler(i)" [src]="thumb" alt="thumb description" />
+            </li>
+        </ul>
+    `,
+    styleUrls: ['./thumb.component.css']
+})
+
+export class ThumbComponent {
+    @Output()
+    public promoIndex: EventEmitter<number> = new EventEmitter();
+
+    private current = 0;
+
+    private thumbs: string[] = [
+        '../../assets/thumb01.jpg',
+        '../../assets/thumb02.jpg',
+        '../../assets/thumb03.jpg',
+        '../../assets/thumb04.jpg'
+    ];
+
+    public clickHandler(index): void {
+        this.current = index;
+        this.promoIndex.emit(index);
+    }
+}
